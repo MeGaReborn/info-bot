55aa241273b13aae83ad3f81b2d9245fefcd9ba9
MeGaReborn <rezabayat594@gmail.com>
2017-04-04 15:31:57 +0430

    Update info.php

--- info.php
+++ info.php
@@ -1,5 +1,5 @@
 <?php
-$botToken = " "; // توكن البوت
+$botToken = " "; // توکن
 $website = "https://api.telegram.org/bot".$botToken;
 $update = file_get_contents('php://input');
 $update = json_decode($update, TRUE);
